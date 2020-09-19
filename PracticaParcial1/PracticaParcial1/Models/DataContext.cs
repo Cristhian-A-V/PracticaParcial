1cd2f4f0d5ef612e89b553fdd96567d4308edf68
Cristhian Arispe Vargas <a2019114216@estudiantes.upsa.edu.bo>
2020-09-18 21:59:09 -0400

    Con controlador de student + autorizacion

---
+++ PracticaParcial1/PracticaParcial1/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace PracticaParcial1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<PracticaParcial1.Models.Student> Students { get; set; }
+    }
+}
