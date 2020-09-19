f603b388224ac05b014f1c8c3dafddd739e1c21c
Cristhian Arispe Vargas <a2019114216@estudiantes.upsa.edu.bo>
2020-09-19 08:48:36 -0400

    Se incluyo un proyecto de unit test

---
+++ PracticaParcial1/UTPracticaParcial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UTPracticaParcial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTPracticaParcial")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("032ae739-eed1-41c7-9d85-32935474c0bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PracticaParcial1/UTPracticaParcial/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticaParcial1.Controllers;
+
+namespace UTPracticaParcial
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController();
+            //Act
+            var ListaEstudiante = studentsController.GetStudents();
+            //Assert
+            Assert.IsNotNull(ListaEstudiante);
+        }
+    }
+}
